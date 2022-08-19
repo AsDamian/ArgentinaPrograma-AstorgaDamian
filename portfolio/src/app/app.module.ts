504dbd21bb343a0c974db123d0e199ee07b6b6db
AsDamian <100476627+AsDamian@users.noreply.github.com>
2022-08-18 21:34:13 -0300

    Header

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgComponent } from './components/logo-arg/logo-arg.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
